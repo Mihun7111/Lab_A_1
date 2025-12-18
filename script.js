02632668d76c774da5ef04e5345ceefcf038100b
mihun7111 <n7792868@gmail.com>
2025-12-18 16:34:27 +0800

    選擇器_完

--- script.js
+++ script.js
@@ -8,6 +8,40 @@ let imageArea = document.getElementById('image-area');
 let regionImage = document.getElementById('region-image');
 let imageCaption = document.getElementById('image-caption');
 
+function clearSelect() {
+    for (let r of document.querySelectorAll(".county, .county-hit")) {
+        r.classList.remove("selected");
+    }
+}
+
+function activate(countyData) {
+    let name = countyData.getAttribute('data-county-name');
+    let pop = countyData.getAttribute('data-population');
+    let feature = countyData.getAttribute('data-feature');
+    let img = countyData.getAttribute('data-img');
+    let captionText = countyData.getAttribute('data-caption');
+
+    let county = countyData.querySelector(".county");
+    let hitCounty = countyData.querySelector(".county-hit");
+
+    clearSelect();
+
+    if (county) county.classList.add("selected");
+    if (hitCounty) hitCounty.classList.add("selected");
+
+    cardInfo.style.opacity = '1';
+    cardInfo.classList.add("selected");
+    cardInfo.innerHTML = `
+        <h2>${name}</h2>
+        <p>人口: ${pop}</p>
+        <p>特點: ${feature}</p>
+    `;
+
+    imageArea.classList.add("visible");
+    regionImage.src = img;
+    imageCaption.textContent = captionText;
+}
+
 for (let countyData of countyEl) {
     let name = countyData.getAttribute('data-county-name');
     let pop = countyData.getAttribute('data-population');
@@ -18,51 +52,24 @@ for (let countyData of countyEl) {
     let hitCounty = countyData.querySelector(".county-hit");
     console.log(county);
 
-    function clearSelect() {
-        for (let r of document.querySelectorAll(".county, .county-hit")) {
-            r.classList.remove("selected");
-        }
-    }
-
-    function selected() {
-        let img = countyData.getAttribute('data-img');
-        let captionText = countyData.getAttribute('data-caption');
-
-        clearSelect();
-
-        county.classList.add("selected");
-        if (hitCounty) {
-            hitCounty.classList.add("selected");
-        }
-
-        cardInfo.style.opacity = '1';
-        cardInfo.classList.add("selected");
-        cardInfo.innerHTML = `
-            <h2>${name}</h2>
-            <p>人口: ${pop}</p>
-            <p>特點: ${feature}</p>
-        `;
-
-        imageArea.classList.add("visible");
-        regionImage.src = img;
-        imageCaption.textContent = captionText;
-
+    function clicked() {
+        activate(countyData);
+        regionSelect.value = name;
     }
 
     if (county) {
-        county.addEventListener("click", selected);
+        county.addEventListener("click", clicked);
     }
     if (hitCounty) {
-        hitCounty.addEventListener("click", selected);
+        hitCounty.addEventListener("click", clicked);
     }
 }
 console.log(allCountyData);
 
-
 let regionSelect = document.getElementById('regionSelect');
-regionSelect.addEventListener('change', function (e) {
+function changeSelect(e) {
     let val = e.target.value;
-
+    console.log(val);
 
     if (!val) {
         clearSelect()
@@ -75,7 +82,7 @@ regionSelect.addEventListener('change', function (e) {
     let target = document.querySelector(`g[data-county-name="${val}"]`);
     console.log(target);
     if (target) {
-        let county = target.querySelector('.county');
-        county.dispatchEvent(new Event('click'));
+        activate(target);
     }
-});
+}
+regionSelect.addEventListener('change', changeSelect);
